03f3836315921c0bf5d324aa0b4fb56c33890e1b
Christian Emmanuel Ramirez medina <lemardelamar@gmail.com>
2022-06-23 05:04:19 -0500

    Eliminado la clase

--- archivo1.cpp
+++ archivo1.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 #include <cstring>
-#include "archivo2.hpp"
 
 using namespace std;
 
 int main(){
 
-    chanchitos* chancho = new chanchitos(true);
+    cout<<"Chanchito feliz"<<endl;
+    cout<<"Chanchito feliz"<<endl;
 
-    if(chancho -> getEstadoAnimo() == true){
-        cout<<"Chanchito feliz"<<endl;
-    }
-    else
-    {
-        cout<<"Chanchito triste"<<endl;
-    }
+    
 
     EXIT_SUCCESS;
 }
